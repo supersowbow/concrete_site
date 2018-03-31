141ddfc8d56ed0c0794493c00b501e66836d82fb
supersowbow <kybow@protonmail.com>
2018-03-31 17:09:20 -0500

    added new image and updated navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-// Give Navbar background color once scrolled
-// $(window).scroll(function() {
-//     if ($(document).scrollTop() > 100) /*height in pixels when the navbar becomes non opaque*/ {
-//         $("#header-container").addClass("scrolled");
-//     } else {
-//         $("#header-container").removeClass("scrolled");
-//     }
-// });
+//Give Navbar background color once scrolled
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) /*height in pixels when the navbar becomes non opaque*/ {
+        $("#header-container").addClass("scrolled");
+    } else {
+        $("#header-container").removeClass("scrolled");
+    }
+});
